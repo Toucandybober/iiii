f18e0bba32a39efb59d554175df2068a3d80ceab
Toucandybober <Blagg505@gmail.com>
2022-12-04 20:06:06 +0700

    added main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float num, sq;
+	int pow;
+	cin >> num;
+	sq = num;
+	cin >> pow;
+	for (int i = 1; i < pow; i++)
+		sq = sq * num;
+	cout << endl << sq;
+}
+
+
